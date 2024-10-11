305db86402ba5655718a8cca39b9b76f767047d1
ezeut <ezeut@naver.com>
2024-10-11 21:43:34 +0900

    Set up UI Structure and basic features for HomeViewController

--- Dding/Views/TabViewController.swift
+++ Dding/Views/TabViewController.swift
@@ -7,22 +7,20 @@
 
 import UIKit
 
-class ViewController: UITabBarController {
+class TabViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         
         let homeVC = HomeViewController()
         homeVC.tabBarItem = UITabBarItem(title: "오늘 할 일", image: UIImage(systemName: "house"), tag: 0)
 
         let calendarVC = SubjectViewController()
-        calendarVC.tabBarItem = UITabBarItem(title: "전체보기", image: UIImage(systemName: "square.grid.2x2"), tag: 1)
+        calendarVC.tabBarItem = UITabBarItem(title: "모아보기", image: UIImage(systemName: "square.grid.2x2"), tag: 1)
 
         let tabBarList = [homeVC, calendarVC]
         viewControllers = tabBarList
     }
-
 
 }
 
--- Dding/Views/TodoListCell.swift
+++ Dding/Views/TodoListCell.swift
@@ -8,51 +8,118 @@
 import UIKit
 
 class TodoListCell: UITableViewCell {
-
-    // UI 요소들
-    private let titleLabel = UILabel()
-    private let checkmarkImageView = UIImageView()
-
+    
+    private lazy var emojiView: UIView = {
+        let view = UIView(frame: .init(x: 0, y: 0, width: 16, height: 16))
+        view.layer.cornerRadius = 8
+        
+        return view
+    }()
+    
+    private lazy var stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.spacing = 8
+        
+        return stackView
+    }()
+    
+    private lazy var todoTitleLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 12)
+        label.textColor = .secondaryLabel
+        
+        return label
+    }()
+    
+    private lazy var checkImageView: UIImageView = {
+        let imageView = UIImageView()
+        
+        return imageView
+    }()
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        contentView.frame = contentView.frame.inset(by: UIEdgeInsets(top: 0, left: 16, bottom: 16, right: 16))
+    }
+    
+    func configure() {
+        contentView.backgroundColor = .yellow
+        contentView.layer.cornerRadius = 8
+        addSubviews()
+        makeConstraints()
+    }
+    
+    private func addSubviews() {
+        contentView.addSubview(emojiView)
+        contentView.addSubview(stackView)
+        stackView.addArrangedSubview(todoTitleLabel)
+        stackView.addArrangedSubview(checkImageView)
+    }
+    
+    private func makeConstraints() {
+        emojiView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        todoTitleLabel.translatesAutoresizingMaskIntoConstraints = false
+        checkImageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            emojiView.centerYAnchor.constraint(equalTo: centerYAnchor),
+            emojiView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
+            emojiView.widthAnchor.constraint(equalToConstant: 16),
+            emojiView.heightAnchor.constraint(equalToConstant: 16),
+            
+            stackView.centerYAnchor.constraint(equalTo: centerYAnchor),
+            stackView.leadingAnchor.constraint(equalTo: emojiView.trailingAnchor, constant: 4),
+            stackView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: 8),
+        ])
+        todoTitleLabel.setContentCompressionResistancePriority(.defaultHigh, for: .horizontal)
+        checkImageView.setContentHuggingPriority(.defaultHigh, for: .horizontal)
+    }
+    
+    
+    //    private let titleLabel = UILabel()
+    //    private let checkmarkImageView = UIImageView()
+    //
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        setupUI() // 셀의 UI를 설정
     }
-
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
-    // 셀 UI 설정
-    private func setupUI() {
-        addSubview(titleLabel)
-        addSubview(checkmarkImageView)
-
-        titleLabel.translatesAutoresizingMaskIntoConstraints = false
-        checkmarkImageView.translatesAutoresizingMaskIntoConstraints = false
-
-        NSLayoutConstraint.activate([
-            titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
-            titleLabel.centerYAnchor.constraint(equalTo: centerYAnchor),
-
-            checkmarkImageView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
-            checkmarkImageView.centerYAnchor.constraint(equalTo: centerYAnchor),
-            checkmarkImageView.widthAnchor.constraint(equalToConstant: 24),
-            checkmarkImageView.heightAnchor.constraint(equalToConstant: 24)
-        ])
-    }
-
-    // 셀을 구성하는 메서드 (모델 데이터로 UI 설정)
+    //
+    //    private func setupUI() {
+    //        addSubview(titleLabel)
+    //        addSubview(checkmarkImageView)
+    //
+    //        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+    //        checkmarkImageView.translatesAutoresizingMaskIntoConstraints = false
+    //
+    //        NSLayoutConstraint.activate([
+    //            titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
+    //            titleLabel.centerYAnchor.constraint(equalTo: centerYAnchor),
+    //
+    //            checkmarkImageView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
+    //            checkmarkImageView.centerYAnchor.constraint(equalTo: centerYAnchor),
+    //            checkmarkImageView.widthAnchor.constraint(equalToConstant: 24),
+    //            checkmarkImageView.heightAnchor.constraint(equalToConstant: 24)
+    //        ])
+    //    }
+    //    override func layoutSubviews() {
+    //        super.layoutSubviews()
+    //        contentView.frame = contentView.frame.inset(by: UIEdgeInsets(top: 16.0, left: 16, bottom: 16, right: 16))
+    //    }
+    //
     func configure(with item: TodoItem) {
         
         if item.isCompleted {
-            // 완료된 항목의 스타일 설정 (예: 취소선)
             textLabel?.textColor = .lightGray
             textLabel?.attributedText = NSAttributedString(string: item.title, attributes: [.strikethroughStyle: NSUnderlineStyle.single.rawValue])
         } else {
-            // 미완료 항목의 스타일 설정
             textLabel?.textColor = .black
             textLabel?.attributedText = NSAttributedString(string: item.title, attributes: [:])
         }
-        checkmarkImageView.image = item.isCompleted ? UIImage(systemName: "checkmark.circle") : UIImage(systemName: "circle")
+        checkImageView.image = item.isCompleted ? UIImage(systemName: "checkmark.circle") : UIImage(systemName: "circle")
     }
 }
